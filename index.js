c17c38b75e791da615344ac153bc1169bcc80932
KaterinaGerts <75572090+KaterinaGerts@users.noreply.github.com>
2021-05-24 10:50:08 +0300

    начало дз

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import pictures from './gallery-items.js';
 
+ const galleryImagesEl = document.querySelector('.js-gallery');
+// console.log(galleryImagesEl);
+const makeGalleryPicturesMarkup = createPicturesMarkup(pictures);
+ 
+galleryImagesEl.insertAdjacentHTML('beforeend', makeGalleryPicturesMarkup);
+
+galleryImagesEl.addEventListener('click', onGalleryPicturecClick);
+
 console.log(createPicturesMarkup(pictures));
 
  function createPicturesMarkup(pictures) {
-   const markup = pictures.map(({ preview, original, description }) => {     
-    return `
+   return pictures.map(({ preview, original, description }) => {
+     return `
   <li class="gallery__item">
     <a
       class="gallery__link"
@@ -18,13 +26,42 @@ console.log(createPicturesMarkup(pictures));
     </a>
 </li>
  `;
-   })
-   console.log(markup);   
+   }).join('');
   }
 
+function onGalleryPicturecClick(e) {
+  const galleryLinkEl = e.target.classList.contains('gallery__link');
+  if (!galleryLinkEl) {
+    return;
+  }
+  const currentPicture = document.querySelector('.gallery__link.is-open')
+
+  if (currentPicture) {
+    currentPicture.classList.remove('is-open')
+  }
+
+  const linkEl = e.target;
+  const parentGalleryLink = linkEl.closest('.gallery__link');
+
+  parentGalleryLink.classList.add('is-open');
+  
+  }
+ 
 
 
 
 
-// const galleryImagesEl = document.querySelector('.js-gallery');
-// console.log(galleryImagesEl);
+
+
+// const refs = {
+//   overlayEl: document.querySelector('lightbox'),
+//   closeBtn: document.querySelector('[data-action="close-lightbox"]')
+// }  
+
+// refs.overlayEl.addEventListener('click', onLightBoxClose);
+
+// function onLightBoxClose(e) {
+//  e.target.classList.add('is-open');
+// }
+
+
